e1d827df70a42be603ece3940d7fad4ad02f3f61
stupachok <103885147+stupachok@users.noreply.github.com>
2024-04-24 16:56:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import ______
+import pygame
 import random
+import time
 
 pygame.init()
 
-
-BLACK = _______
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 screen_width = 800
 screen_height = 600
-screen = pygame.display.____((screen_width, screen_height))
-pygame.display.set_caption("_____")
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("guess the right cup")
 
-cup_img = pygame.image.load("___")
+cup_img = pygame.image.load("cup.png")
 class Cup:
-    def __________(self, x, y, image):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
         self.image = image
@@ -24,7 +24,7 @@ class Cup:
         self.has_ball = False
 
     def draw(self):
-        screen._____(self.image, self.rect)
+        screen.blit(self.image, self.rect)
 
     def check_for_ball(self):
         return self.has_ball
@@ -33,27 +33,49 @@ cups = []
 cup_width = 100
 cup_height = 150
 margin = 130
-for i in range(3):
+amount_of_cups = 3
+
+for i in range(amount_of_cups):
     x = margin + i * (cup_width + margin)
     y = screen_height - cup_height - margin
-    cup = ____(x, y, cup_img.copy())
+    cup = Cup(x, y, cup_img.copy())
     cups.append(cup)
 
-ball_cup_index = _____.randint(0, len(cups) - 1)
-cups[________].has_ball = True
+ball_cup_index = random.randint(0, len(cups) - 1)
+cups[ball_cup_index].has_ball = True
+
 
 running = True
 
-while _____:
+
+
+while running:
     for e in pygame.event.get():
-        if e.type == pygame._______:
+        if e.type == pygame.QUIT:
             running = False
+        
+        if e.type == pygame._____:
+             mouse_x, mouse_y = pygame.mouse.get_pos()
+             for cup in ____:
+                 if cup.rect.collidepoint(mouse_x, mouse_y):
+                     if ____.check_for_ball():
+                         cup.image = pygame.image._____("cup_and_ball.png")
+                     else:
+                         cup.image = pygame.____.load("cup_and_nothing.png")
+   
 
-    screen._____(WHITE)
 
-    for cup in ______:
+    screen.fill(WHITE)
+
+
+
+    for cup in cups:
         cup.draw()
 
-    pygame._____.flip()
+    
+    score_text = font.render(f'Score: {score}', True, BLACK)
+    screen.blit(score_text, (10, 10))
+
+    pygame.display.flip()
 
 pygame.quit()
