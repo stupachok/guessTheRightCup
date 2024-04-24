c90918e6da3d8a4b5d7e4a2e2b80b5474243478d
stupachok <103885147+stupachok@users.noreply.github.com>
2024-04-24 17:13:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("guess the right cup")
 
 cup_img = pygame.image.load("cup.png")
+
+def reset_game():
+     global won, ball_cup_index
+     for cup in cups:
+         cup.has_ball = False
+         cup.____ = cup_img.copy()
+     ball_cup_index = random.____(0, len(cups) - 1)
+     cups[_____].has_ball = True
+     won = False
+
 class Cup:
     def __init__(self, x, y, image):
         self.x = x
@@ -44,25 +54,31 @@ for i in range(amount_of_cups):
 ball_cup_index = random.randint(0, len(cups) - 1)
 cups[ball_cup_index].has_ball = True
 
+font = _____.font.Font(None, 36)
+button_text = font.render('Start Over', True, BLACK)
+button_rect = _______.get_rect(center=(screen_width // 2, 100))
 
 running = True
-
-
+won = ______
 
 while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
         
-        if e.type == pygame._____:
+        if e.type == pygame.MOUSEBUTTONDOWN:
              mouse_x, mouse_y = pygame.mouse.get_pos()
-             for cup in ____:
+             for cup in cups:
                  if cup.rect.collidepoint(mouse_x, mouse_y):
-                     if ____.check_for_ball():
-                         cup.image = pygame.image._____("cup_and_ball.png")
+                     if cup.check_for_ball():
+                         cup.image = pygame.image.load("cup_and_ball.png")
+                         won = _____
                      else:
-                         cup.image = pygame.____.load("cup_and_nothing.png")
-   
+                         cup.image = pygame.image.load("cup_and_nothing.png")
+        
+        if e.type == pygame.MOUSEBUTTONDOWN and won:
+             if button_rect.collidepoint(pygame.mouse.get_pos()):
+                 reset_game()
 
 
     screen.fill(WHITE)
@@ -73,8 +89,15 @@ while running:
         cup.draw()
 
     
-    score_text = font.render(f'Score: {score}', True, BLACK)
-    screen.blit(score_text, (10, 10))
+    
+    if won:
+         font = pygame.font.Font(None, 72)
+         victory_text = font.render('You won!', True, ____)
+         text_rect = victory_text.get_rect(center=(screen_width // 2, screen_height // 2 - 100))
+         screen.______(victory_text, text_rect)
+         pygame.draw.rect(screen, WHITE, button_rect)
+         ______.blit(button_text, button_rect.topleft)
+    
 
     pygame.display.flip()
 
